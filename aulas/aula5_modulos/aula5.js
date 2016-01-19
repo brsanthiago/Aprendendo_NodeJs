3e64ef32c80b4c08b13a3f0c001dcb649c5e25a9
Bruno Santiago <brsanthiago@users.noreply.github.com>
2016-01-19 20:31:50 -0200

    Update aula5.js

--- aulas/aula5_modulos/aula5.js
+++ aulas/aula5_modulos/aula5.js
@@ -1,2 +1,7 @@
 var modulo = require('./module/modulo1');
-modulo('Bruno');
+modulo('Xico Xavier')
+
+var module = require('./module/modulo2');
+module.minhaIdade(75);
+module.minhaProfissao('Mediun / Medium / Sei la como escreve.');
+module.estadoCivil('Não interessa pra você palhaço');
