d74dcfa804f1e7b5f6500047873a70165496e2ac
Bruno Santiago <brsanthiago@users.noreply.github.com>
2016-01-19 19:51:23 -0200

    Escrevendo em arquivo texto com Nodejs

--- aulas/aula3.js
+++ aulas/aula3.js
@@ -1,10 +1,22 @@
 var arquivo = require('fs');
-arquivo.readFile('./clubes/clubes.txt',function(err,data){
+var localEscrita = './clubes/2ª-divisao.txt';
+var localLeitura = './clubes/clubes.txt';
+
+//Ler arquivo
+arquivo.readFile(localLeitura,function(err,data){
 	if(err) throw err;
 	console.log(data.toString());
 });
 
-arquivo.writeFile('./clubes/2divisao.txt','Vasco', function(err){
-	if(err) throw err;
-	console.log('Escreve Times na Segunda Divisão!');
-})
+//Verifica se existe arquivo
+arquivo.exists(localEscrita,function(resultado){
+	if(!resultado){
+		//Escrevendo em arquivo
+		arquivo.writeFile(localEscrita,'Vasco', function(err){
+			if(err) throw err;
+			console.log('Escreve Times na Segunda Divisão!');
+		});		
+	}else{
+		console.log('Arquivo já existe');
+	}
+});
