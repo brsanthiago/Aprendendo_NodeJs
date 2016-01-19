7c28898996cf534f6a33f3996471465d1d178714
Bruno Santiago <brsanthiago@users.noreply.github.com>
2016-01-19 20:33:53 -0200

    Create modulo2.js

---
+++ aulas/aula5_modulos/module/modulo2.js
@@ -0,0 +1,11 @@
+exports.minhaIdade = function(idade){
+	console.log("Minha Idade é: "+ idade);
+};
+
+exports.minhaProfissao = function(profissao){
+	console.log('Minha Profissão é: '+ profissao);
+};
+
+exports.estadoCivil = function(estadoCivil){
+	console.log('Meu Estado Civil é: '+estadoCivil);
+};
